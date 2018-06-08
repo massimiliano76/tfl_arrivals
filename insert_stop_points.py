84c478a0ebfd482323bb98164fd6bcd3214a6c9b
Attila Mitrocsak <attila.mitrocsak@gmail.com>
2018-06-08 20:28:09 +0100

    Ignore minor errors in the data

--- insert_stop_points.py
+++ insert_stop_points.py
@@ -13,7 +13,7 @@ def main():
     session_factory = sessionmaker(bind=engine)
     session = scoped_session(session_factory)
     db.create_all()
-    stop_file = open("stop_point_naptan_ids2", "r")
+    stop_file = open("stop_point_naptan_ids_all", "r")
     all_stops = [l.strip() for l in stop_file.readlines()]
     count = 0
     for file_naptan in all_stops:
@@ -24,13 +24,18 @@ def main():
             db_stop = session.query(StopPoint).filter(StopPoint.naptan_id == stop.naptan_id).one_or_none()
             if db_stop is not None:
                 assert(db_stop.naptan_id == stop.naptan_id)
-                assert(db_stop.name == stop.name)
-                assert(db_stop.latitude == stop.latitude)
-                assert(db_stop.longitude == stop.longitude)
+                if stop.naptan_id not in ["490015502P", "490016293W", "490005198F",  "490005368N", "490000984X", "490000984Y"]:
+                    assert(db_stop.name == stop.name)
+                if stop.naptan_id not in ["940GZZLUBNK", "940GZZLUEUS", "940GZZLUICK", "490015114E", "490010100E", "940GZZCRKGH"]:
+                    assert(db_stop.latitude == stop.latitude)
+                    assert(db_stop.longitude == stop.longitude)
+
                 if stop.sms_code != None and stop.sms_code != "" and \
                     db_stop.sms_code != stop.sms_code:
                     print(f"New sms code found for {stop.naptan_id}: {stop.sms_code}")
                     db_stop.sms_code = stop.sms_code
+                db_stop.lines_bus = stop.lines_bus
+                db_stop.lines_tube = stop.lines_tube
             else:
                 session.add(stop)
             session.commit()
