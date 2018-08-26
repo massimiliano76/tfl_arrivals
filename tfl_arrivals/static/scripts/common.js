f535144e9aec704d7587e42f33474d68ff66c192
Attila Mitrocsak <attila.mitrocsak@gmail.com>
2018-08-26 12:07:43 +0100

    Removed temporary hardcoded api address

--- tfl_arrivals/static/scripts/common.js
+++ tfl_arrivals/static/scripts/common.js
@@ -2,6 +2,17 @@
 function api_host() {
     if (window.location.hostname == "localhost")
         return "http://localhost:5000";
+    else if (window.location.hostname == "127.0.0.1")
+        return "http://127.0.0.1:5000";
     else
         return "http://" + window.location.hostname;
+}
+
+function api_host_gcp() {
+    if (window.location.hostname == "localhost")
+        return "http://localhost:8080";
+    else if (window.location.hostname == "127.0.0.1")
+        return "http://127.0.0.1:8080";
+    else
+        return "https://arrivals-of-london.appspot.com";
 }
